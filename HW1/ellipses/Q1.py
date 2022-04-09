2e11cc450d94e779d171297ac5e8be9f88c671bd
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-04-10 01:54:55 +0300

    Calculated gradient map

--- HW1/ellipses/Q1.py
+++ HW1/ellipses/Q1.py
@@ -2,6 +2,8 @@ import cv2.cv2 as cv2
 import glob
 from copy import deepcopy
 
+import numpy as np
+
 if __name__ == "__main__":
     i_types = ('*.png', '*.webp', '*.jpg', '*.jpeg')  # tuple of all images types in the folder
     images_names = []
@@ -22,7 +24,7 @@ if __name__ == "__main__":
         elif im_name == "72384675-very-long-truck-trailer-for-exceptional-transport-with-many-sturdy-tires.webp":
             edge_map = cv2.Canny(im, 50, 255)
         elif im_name == "1271488188_2077d21f46_b.jpg":
-            edge_map = cv2.Canny(im, 100, 200)
+            edge_map = cv2.Canny(im, 70, 150)
         elif im_name == "alvtd333_alvin_template_small_ellipse.png":
             edge_map = cv2.Canny(im, 100, 200)
         elif im_name == "gettyimages-1212455495-612x612.jpg":
@@ -36,13 +38,17 @@ if __name__ == "__main__":
         elif im_name == "nEKGD2wNiwqrTOc63kiWZT7b4.png":
             edge_map = cv2.Canny(im, 150, 200)
         elif im_name == "s-l400.jpg":
-            edge_map = cv2.Canny(im, 100, 200)
+            edge_map = cv2.Canny(im, 100, 180)
         elif im_name == "sewage-treatment-plant-wastewater-treatment-water-use-filtration-effluent-and-waste-water" \
                         "-industrial-solutions-for-sewerage-water-treatment-and-rec-2H4RX20.jpg":
             edge_map = cv2.Canny(im, 100, 200)
         else:  # "Traitement-dimage-drone-.jpeg":
             edge_map = cv2.Canny(im, 100, 200)
 
+        gradient_map = cv2.Laplacian(edge_map, cv2.CV_64F)
+        gradient_map = np.absolute(gradient_map)
+
         cv2.imshow('image', im)
         cv2.imshow('edge map', edge_map)
+        cv2.imshow('gradient', gradient_map)
         cv2.waitKey(0)
